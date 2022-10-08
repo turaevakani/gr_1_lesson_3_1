f162e424364e81fe927081d1649dea77098b817e
turaevakani <kanykeituraeva03@gmail.com>
2022-10-08 22:44:51 +0600

    hi

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,26 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double[] massive = {2, -5, 16, -62, -18, 74, 32, 85, 14, 36, 55, -41, -60, 25 };
-        double average = 0.0;
-        int count = 0;
-        for(int i=0; i<massive.length; i++){
-            if(massive[i]<0){
-                for (int j = i+1; j <massive.length ; j++) {
-                    if(massive[j]>0){
-                        average += massive[j];
-                        count++;
-                    }
-                }
-                break;
+        double[] massive = {2.22, -5.445, 16.778, -62.3, -18.74, 74.454, 32.21, 85.44, 14.01, 36.12, 55.74, -41.2, -60.3, 25.25 };
+        boolean a = false;
+        double sumOfmassive = 0;
+        int indexOfElements = 0;
+        for(double elementsOfMassive : massive){
+            if(elementsOfMassive<0){
+                a=true;
+            }
+            if(elementsOfMassive>0 && a){
+                System.out.println(elementsOfMassive);
+                sumOfmassive +=elementsOfMassive;
+                indexOfElements++;
             }
         }
-        System.out.println(Arrays.toString(massive) + "=" + average/count);
+        System.out.println(sumOfmassive + "/" + indexOfElements +  "=" + sumOfmassive/indexOfElements);
 
 
 
-        
+
+
         System.out.println("***********************");
         System.out.println(Arrays.toString(massive));
         boolean sorted = false;
